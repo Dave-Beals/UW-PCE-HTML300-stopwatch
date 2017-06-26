7f4d7ac764885ba8155c735bc4b58997098b0413
Dave <dbeals85@gmail.com>
2017-06-26 00:05:35 -0700

    initial commit

--- stopwatch.js
+++ stopwatch.js
@@ -1,3 +1,19 @@
+// button controls
+const start = document.querySelector('button.start')
+const stop = document.querySelector('button.stop')
+
+//DOM elements that I need to update
+const lapList = document.querySelector('#lapList') //finds first element with id "lapList"
+const stopwatchTime = document.querySelector('#stopwatchTime')
+
+//constants that shouldn't ever change
+const laps = []
+const intervalRate = 10 //update the stopwatch every 10 milliseconds
+
+//values that will change pretty often
+let intervalId = null // variable for an interval. this is just a place to put it
+let rawTime = 0
+
 // turns the time into a human readable format
 function formatTime (raw) {
   let seconds = Math.floor(raw / 1000)
@@ -8,6 +24,31 @@ function formatTime (raw) {
   return `${zeroPad(minutes)}:${zeroPad(seconds.toFixed(2))}`
 }
 
+//start the stopwatch by creating a new interval
+//we'll store the interval so we can manipulate the interval later
+function stopwatchStart(event) {
+  event.preventDefault()
+  console.log('started!')
+
+  //every 10 milliseconds, update the stopwatch
+  intervalId = setInterval(stopwatchUpdate, intervalRate)
+}
+
+//adds the interval to the stopwatch time since the last "tick"
+//then update the DOM with the new stopwatch time
+function stopwatchUpdate() {
+  rawTime += intervalRate
+  stopwatchTime.innerHTML = formatTime(rawTime)
+}
+
+//stops the stopwatch by clearing the interval
+function stopwatchStop(event) {
+  event.preventDefault()
+  console.log('stopped!')
+
+  clearInterval(intervalId)
+}
+
 // adds a leading zero because humans like them
 function zeroPad (value) {
   let pad = value < 10 ? '0' : ''
@@ -16,4 +57,8 @@ function zeroPad (value) {
 
 document.addEventListener("DOMContentLoaded", function () {
   console.log('ready!')
+
+//below is -> eventListener is waiting on a click on the start button to pass on the stopwatchStart function. Line below is same but for stop
+  start.addEventListener("click", stopwatchStart)
+  stop.addEventListener("click", stopwatchStop)
 })
